e97b1f410bcaf4ecb563d41544ff4c404f76a824
MarcoDT02 <mditerlizzi02@gmail.com>
2026-08-31 18:39:10 +0200

    implement JWT authentication and protect product routes

--- app.py
+++ app.py
@@ -1,17 +1,21 @@
 from flask import Flask
+from flask_jwt_extended import JWTManager
 from models import db
 from routes import api_bp
 
 app = Flask(__name__)
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://postgres:PASSWORD@localhost:5432/eshop_db" # Change PASSWORD ----------------
+app.config["SQLALCHEMY_DATABASE_URI"] = ("postgresql://postgres:PASSWORD@localhost:5432/eshop")  # Change PASSWORD ----------------
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+app.config["JWT_SECRET_KEY"] = ("8f3b2a9e4c1d6f7a8b9c0d1e2f3a4b5c6d7e8f9a0b1c2d3e4f5a6b7c8d9e0f1a")
+jwt = JWTManager(app)
 
 db.init_app(app)
 app.register_blueprint(api_bp)
 
 with app.app_context():
-    db.create_all()
+  db.create_all()
 
 if __name__ == "__main__":
-    app.run(debug=True)
+  app.run(debug=True)
